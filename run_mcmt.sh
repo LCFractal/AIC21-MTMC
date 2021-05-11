45c1b37028203092413e058262737cb644447494
LCFractal <lc-fractal@hotmail.com>
2021-05-11 12:09:09 +0800

    Update run_mcmt.sh
    fix detection dir

--- run_mcmt.sh
+++ run_mcmt.sh
@@ -1,7 +1,7 @@
 MCMT_CONFIG_FILE="aic_mcmt.yml"
 #### Run Detector.####
 cd detector/
-#python gen_images_aic.py ${MCMT_CONFIG_FILE}
+python gen_images_aic.py ${MCMT_CONFIG_FILE}
 
 cd yolov5/
 #sh gen_det.sh ${MCMT_CONFIG_FILE}
